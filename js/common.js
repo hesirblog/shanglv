a70144b18863bd1d03190c6a3eda22fe338bcc78
hesir <hetianxing668@163.com>
2013-06-11 23:36:13 +0800

    20130611-01

--- js/common.js
+++ js/common.js
@@ -1,3 +1,13 @@
-$(document).ready(function(){
+//分类切换
 
-});
+
+//选项卡切换
+function ChangeTab(i,j)
+{
+	var tab_title = $(".tab_news").eq(i).find("ul.tab_title li");
+	var tab_content = $(".tab_news").eq(i).find("ul.tab_content");
+	tab_title.removeClass("cur");
+	tab_title.eq(j).addClass("cur");
+	tab_content.hide();
+	tab_content.eq(j).show();
+}
